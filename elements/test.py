43cecc7f3ffb70c8d23c3e01eefb4e0246f592e8
Jessica Lartiorkor Lartey <jessica.lartey@amalitech.com>
2023-04-25 16:28:18 +0000

    Add sample file

---
+++ elements/test.py
@@ -0,0 +1,11 @@
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).parent
+
+# image_path = BASE_DIR / 'sampleFile.jpeg'
+
+# print(image_path)
+# print(f"Type: {type(image_path)}")
+
+# print(str(image_path))
+# print(f"Type 2: {type(str(image_path))}")
